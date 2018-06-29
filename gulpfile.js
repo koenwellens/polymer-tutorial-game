cd2d3e9382921f0648db094ff89084722ef55e64
koenwellens <koen.wellens@hotmail.com>
2018-06-29 21:04:56 +0200

    Edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,9 @@ const gulp = require('gulp');
 gulp.task('copy-assets', (done) => {
     gulp.src('static/**/*.{png,json}')
         .pipe(gulp.dest('./build/default/static/'));
+
+    gulp.src('bower_components/ace-builds/src-min-noconflict/{theme-monokai,mode-html}.js')
+        .pipe(gulp.dest('./build/default/bower_components/ace-builds/src-min-noconflict/'));
+
     done();
 });
